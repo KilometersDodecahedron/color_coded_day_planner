8d87ecc8d83941cbbd4d0f1e8a111c0cfbba5b7c
Miles Cohn <milesselim@icloud.com>
2020-07-27 13:45:48 -0400

    I added the assignment. Remember: github will take up to 20 minutes to make the link live

---
+++ README.md
@@ -0,0 +1,25 @@
+#A Day Planner that stores the events you wish to remember for that day.
+
+##The information is Stored in localStorage in the browser
+
+The current day and date is displayed at the top
+
+![Day and Date displayed](./Screenshots/DateDisplay.png)
+
+Hour Time Slots in the **future** are **Green**
+
+![Future Time Slots](./Screenshots/future.png)
+
+Hour Time Slots in the **past** are **Grey** 
+
+![Past Time Slots](./Screenshots/past.png)
+
+and the **current** Hour Time Slots is **Red**
+
+![Current Time Slot](./Screenshots/CurrentTime.png)
+
+**To save** the info you type in the planner, you must press the **Blue Save Button** on the right of the display area
+
+![The Blue Save Button](./Screenshots/saveButton.png)
+
+*Note: this only saves the data in the slot it's attached to, not the whole page*
---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    //html elements
+    var textAreaArray = $(".col-10");
+    var saveButtons = $("button");
+    var dateDisplayElement = $("#currentDay");
+
+    //moment object
+    var currentDateObject = moment();
+
+    var savedNotesArray = [];
+    var loadKeyString = "day-planner-data-for-h5-you-assholes";
+    
+    //show the current date and time
+    $(dateDisplayElement).text(currentDateObject.format('dddd, MMMM Do, YYYY'));
+
+    //set textarea background based on relative time
+    $.each(textAreaArray, function(i, current){
+        var hourValue = $(current).attr("id");
+        if(hourValue > currentDateObject.hour()){
+            $(current).addClass("future");
+        }
+        else if(hourValue < currentDateObject.hour()){
+            $(current).addClass("past");
+        }
+        else
+        {
+            $(current).addClass("present");
+        }
+    })
+
+    $.each(saveButtons, function(i, current){
+        $(current).click(function(){
+            var associatedTextArea = $(current).siblings("[id]")[0];
+
+            savedNotesArray[i] = $(associatedTextArea).val();
+            
+            localStorage.setItem(loadKeyString, JSON.stringify(savedNotesArray));
+        });
+    });
+
+    function loadAndSetData(){
+        var loadedData = localStorage.getItem(loadKeyString);
+
+        //if no saved data is found, create an empty array entry for each element
+        if(loadedData == null){
+            $.each(textAreaArray, function(){
+                savedNotesArray.push("");
+                localStorage.setItem(loadKeyString, JSON.stringify(savedNotesArray));
+            });
+        }
+        else
+        {
+            loadedData = JSON.parse(loadedData);
+            savedNotesArray = loadedData;
+            $.each(textAreaArray, function(i, current){
+                $(current).val(savedNotesArray[i]);
+            });
+        }
+    }
+
+    loadAndSetData();
+});
